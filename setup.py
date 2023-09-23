1d71ea98f1ab948e1efb32efb6656a42d03f859c
Vinay S N <vsn@wayfair.com>
2023-09-23 14:38:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "spcCodes"
-SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "sumanpc2008@gmail.com"
+AUTHOR_USER_NAME = "vsn"
+SRC_REPO = "end-end-ml"
+AUTHOR_EMAIL = "vinaysn.ranjan@gmail.com"
 
 
 setuptools.setup(
